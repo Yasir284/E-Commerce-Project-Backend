3b5e7fcc887c4e7c323e2fb8b40889ec52ffa434
Yasir284 <lambawalay@gmail.com>
2023-01-03 11:34:58 +0530

    created collection schema

---
+++ models/collection.schema.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const collectionSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      require: [true, "Collection name is require"],
+      maxLength: [120, "Collection name must be less than 120 characters"],
+      trim: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("Collection", collectionSchema);
