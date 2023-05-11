d39115b719ede4faa098137c86994b3b632a1d5b
Yasir284 <lambawalay@gmail.com>
2023-05-11 22:37:44 +0530

    added get products and delete products controller

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -4,7 +4,7 @@ import asyncHandler from "../services/asyncHandler";
 import fs from "fs";
 import CustomError from "../utils/customError.js";
 import mongoose from "mongoose";
-import { s3FileUpload } from "../services/imageUploader";
+import { s3FileDelete, s3FileUpload } from "../services/imageUploader";
 import config from "../config";
 
 /**********************************************************************
@@ -13,7 +13,7 @@ import config from "../config";
  @route http://localhost:4000/api/product/add
  @description Create products
  @parameters name, price, description, photos
- @return success message
+ @return success message and objects
  **********************************************************************/
 
 export const addProduct = asyncHandler(async (req, res) => {
@@ -76,4 +76,66 @@ export const addProduct = asyncHandler(async (req, res) => {
       throw new CustomError(err.message || "Something went wrong", 500);
     }
   });
+});
+
+/**********************************************************************
+ @GET_ALL_PRODUCTS
+ @request_type GET
+ @route http://localhost:4000/api/product/get
+ @description Get all products
+ @parameters 
+ @return Products objects
+ **********************************************************************/
+
+export const getAllProducts = asyncHandler(async (_req, res) => {
+  const products = await Product.find();
+
+  if (!products) throw new CustomError("Error in getting products", 400);
+
+  res.status(200).json({ success: true, products });
+});
+
+/**********************************************************************
+ @GET_PRODUCT_BY_ID
+ @request_type POST
+ @route http://localhost:4000/api/product/get/:productId
+ @description Get product by id
+ @parameters productId
+ @return Product object
+ **********************************************************************/
+
+export const getProductById = asyncHandler(async (req, res) => {
+  const { productId } = req.params;
+  const products = await Product.findById(productId);
+
+  if (!products) throw new CustomError("Error in getting product", 400);
+
+  res.status(200).json({ success: true, products });
+});
+
+/**********************************************************************
+ @DELETE_PRODUCT
+ @request_type DELETE
+ @route http://localhost:4000/api/product/delete/:productId
+ @description Delete product by id
+ @parameters productId
+ @return Product object
+ **********************************************************************/
+
+export const deleteProduct = asyncHandler(async (req, res) => {
+  const { productId } = req.params;
+  const products = await Product.findByIdAndDelete(productId);
+
+  if (!products) throw new CustomError("Error in deleting product", 400);
+
+  const deleteImages = await s3FileDelete({
+    bucketName: config.S3_BUCKET_NAME,
+    key: `products/${productId}`,
+  });
+
+  console.log(deleteImages);
+
+  res
+    .status(200)
+    .json({ success: true, message: "Product deleted successfully" });
 });
